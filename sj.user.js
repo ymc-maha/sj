58f0ce0571eadb9b1d0039e9011a77a39cd11020
Manuel Hartmann <manuel.hartmann@ymc.ch>
2014-03-18 17:56:24 +0100

    Fix url

--- sj.user.js
+++ sj.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Serienjunkies Customer
 // @namespace   maha
-// @include     http://serienjunkies.org
+// @include     http://serienjunkies.org/*
 // @grant       none
 // @require     http://code.jquery.com/jquery-2.1.0.min.js
 // @run-at      document-end
-// @version     0.1.0
+// @version     0.1.1
 // @updateURL   https://raw.githubusercontent.com/ymc-maha/sj/master/sj.meta.js
 // @downloadURL https://raw.githubusercontent.com/ymc-maha/sj/master/sj.user.js
 // ==/UserScript==
